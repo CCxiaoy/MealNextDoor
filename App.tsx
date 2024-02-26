95c51d33fb69a4f788282c55c492ae9fbe4b0e14
人菜瘾大 <ccxiaoy@hotmail.com>
2024-02-27 01:14:13 +0800

    feat: allowing add and delete mealItem

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 // import API_URL from @env and rename it to baseUrl
 import {API_URL as baseUrl} from '@env';
 
@@ -16,6 +16,8 @@ function App(): React.JSX.Element {
     lunch: [],
     dinner: [],
   });
+  const [newMealText, setNewMealText] = useState('');
+  const [newMealCategory, setNewMealCategory] = useState('');
 
   const getAllMealListsFromApi = async () => {
     const urls = [
@@ -55,9 +57,14 @@ function App(): React.JSX.Element {
             mealList.breakfast.map(
               (meal: {id: number; name: string, category: string}, i: number) => {
               return (
-                <Text key={meal.id} style={styles.listItem}>
-                  {i + 1 + '. ' + meal.name}
-                </Text>
+                <View key={meal.id} style={styles.listItem}>
+                  <Text>{i + 1 + '. ' + meal.name}</Text>
+                  <Text
+                    onPress={() => deleteMeal(meal.id)}
+                    style={styles.buttons}>
+                      删除
+                  </Text>
+                </View>
               );
             })
           ) : (
@@ -74,9 +81,14 @@ function App(): React.JSX.Element {
             mealList.lunch.map(
               (meal: {id: number; name: string, category: string}, i: number) => {
                 return (
-                  <Text key={meal.id} style={styles.listItem}>
-                    {i + 1 + '. ' + meal.name}
+                  <View key={meal.id} style={styles.listItem}>
+                    <Text>{i + 1 + '. ' + meal.name}</Text>
+                  <Text
+                    onPress={() => deleteMeal(meal.id)}
+                    style={styles.buttons}>
+                      删除
                   </Text>
+                  </View>
                 );
               },
             )
@@ -93,9 +105,14 @@ function App(): React.JSX.Element {
           {mealList.dinner.length > 0 ? (
             mealList.dinner.map((meal: { id: number; name: string, category: string }, i: number) => {
               return (
-                <Text key={meal.id} style={styles.listItem}>
-                  {i + 1 + '. ' + meal.name}
-                </Text>
+                <View key={meal.id} style={styles.listItem}>
+                  <Text>{i + 1 + '. ' + meal.name}</Text>
+                  <Text
+                    onPress={() => deleteMeal(meal.id)}
+                    style={styles.buttons}>
+                      删除
+                  </Text>
+                </View>
               );
             })
           ) : (
@@ -105,6 +122,44 @@ function App(): React.JSX.Element {
       );
     }
   };
+
+  // delete meal from the list, need to pass the meal id as the params in delete request type
+  const deleteMeal = async (mealId: number) => {
+    try {
+      const response = await fetch(`${baseUrl}/api/deleteMealItem/${mealId}`, {
+        method: 'DELETE',
+      });
+      const data = await response.json();
+      console.log(data);
+      getAllMealListsFromApi(); // refresh the meal list
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // add new meal to the list, need to pass the meal name and category as the body in post request type
+  const addNewMeal = async () => {
+    try {
+      const response = await fetch(`${baseUrl}/api/addMealItem`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: newMealText,
+          category: newMealCategory,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      setNewMealText(''); // clear the input box
+      setNewMealCategory(''); // clear the input box
+      getAllMealListsFromApi(); // refresh the meal list
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
 
   return (
     <View style={styles.container}>
@@ -116,6 +171,17 @@ function App(): React.JSX.Element {
       </Text>
       <Text style={{...styles.title, ...styles.line}}>计划-下一顿</Text>
       {getMealElements(mealLists)}
+      <View style={styles.newMealContainer}>
+        <View style={styles.newMealInput}>
+          <TextInput value={newMealText} onChangeText={setNewMealText} style={styles.inputNewMeal} />
+          <TextInput value={newMealCategory} onChangeText={setNewMealCategory} style={styles.inputNewMealCategory} />
+        </View>
+        <Text
+          onPress={addNewMeal}
+          style={[styles.buttons, styles.addButton]}>
+            添加
+        </Text>
+      </View>
     </View>
   );
 }
@@ -138,8 +204,58 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   listItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     fontSize: 16,
     marginBottom: 8,
+  },
+  newMealContainer: {
+    width: '100%',
+    paddingLeft: 16,
+    // set two elements in a row, one is TextInput element, the other is a Text element
+    flexDirection: 'row',
+    // set the two elements in the center of the container
+    alignItems: 'center',
+    // set the space between the two elements
+    justifyContent: 'space-between',
+    // set this container to the bottom of the screen
+    position: 'absolute',
+    bottom: 16,
+  },
+  newMealInput: {
+    flexDirection: 'row',
+    width: '80%',
+  },
+  inputNewMeal: {
+    // Top-left and bottom-left radius of the input box should be 10
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderColor: '#707070',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    fontSize: 8,
+    width: '70%',
+    // height just bigger enough to show the text
+    height: 32,
+  },
+  inputNewMealCategory: {
+    // Top-left and bottom-left radius of the input box should be 10
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    borderColor: '#707070',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    fontSize: 8,
+    width: '30%',
+    height: 32,
+  },
+  buttons: {
+    width: '12%',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  addButton: {
+    marginRight: -16,
   },
 });
 
